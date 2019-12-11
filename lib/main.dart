3a65c22e2d9ccf3ac183329a9fc43949346c754d
Mantab Tadayon <mt.tadayon@gmail.com>
2019-12-11 09:32:46 +0100

    Add profile image

--- lib/main.dart
+++ lib/main.dart
@@ -11,36 +11,12 @@ class MyApp extends StatelessWidget {
         home: Scaffold(
             backgroundColor: Colors.teal[600],
             body: SafeArea(
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Container(
-                    width: 100.0,
-                    color: Colors.red,
-                  ),
-                  new Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Container(
-                        width: 100.0,
-                        height: 100.0,
-                        color: Colors.yellow,
-                      ),
-                      Container(
-                        width: 100.0,
-                        height: 100.0,
-                        color: Colors.green,
-                      )
-                    ],
-                  ),
-                  Container(
-                    height: 100.0,
-                    width: 100.0,
-                    color: Colors.blue,
-                  )
-                ],
-              ),
-            )));
+                child: Column(
+              children: <Widget>[
+                CircleAvatar(
+                    radius: 50.0,
+                    backgroundImage: AssetImage('images/mahtab_image.jpg'))
+              ],
+            ))));
   }
 }
